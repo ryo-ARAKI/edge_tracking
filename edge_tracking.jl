0b203229edca00cabb8fa4cd3197a9642ef13d22
ryo-ARAKI <r_araki@fm.me.es.osaka-u.ac.jp>
2021-11-17 16:49:30 +0100

    Add header comment

---
+++ edge_tracking.jl
@@ -0,0 +1,8 @@
+#=
+Compute the 2D system
+    ẋ = -x + 10 y
+    ẏ = y (10 e^(-0.01 x²) - y) (y - 1)
+and investigate its edge state.
+
+Cf.[Rich Kerswell, "Edge Tracking - Walking the Tightrope"](https://gfd.whoi.edu/wp-content/uploads/sites/18/2018/03/rich8_131207.pdf)
+=#
